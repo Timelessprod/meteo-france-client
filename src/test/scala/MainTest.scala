88da1571130fd375b57561b41cf702683159fb57
Adrien Merat <adrien.merat@epita.fr>
2021-11-27 14:46:30 +0100

    [ADD] Add Api object (not terminated) and token file (empty)

--- src/test/scala/MainTest.scala
+++ src/test/scala/MainTest.scala
@@ -1,7 +1,9 @@
 import org.scalatest.funsuite.AnyFunSuite
+import com.meteofrance.Api
 
 class MainTest extends AnyFunSuite {
-  test("Example") {
-    assert(1 == 1)
+  test("Check user API credentials") {
+    assert(Api.USER_ID != null)
+    assert(Api.USER_KEY != null)
   }
 }
