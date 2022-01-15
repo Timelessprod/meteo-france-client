5c6c4ffa680eda502f13b0914d77d7589f8af0ba
Adrien Merat <adrien.merat@epita.fr>
2022-01-15 22:38:56 +0100

    [EDIT] Add isGRIB method

--- src/main/scala/com/meteofrance/Service.scala
+++ src/main/scala/com/meteofrance/Service.scala
@@ -1,5 +1,25 @@
 package com.meteofrance
 
-object Service {
+import com.typesafe.scalalogging.Logger
 
+import scala.collection.mutable.ListBuffer
+
+/**
+ * Manage received data and parse GRIB to Dataframe
+ */
+object Service {
+  val logger = Logger("Service")
+  var locations: ListBuffer[(Double, Double)] = null
+
+  /**
+   * Check if the given string is a valid GRIB file by checking if the first 4 bytes are "GRIB".
+   * This is not the best way to check as the rest can be full jam binary data but it's a simple
+   * and easy way. Normally the Météo France server may return only GRIB data on our calls.
+   * 
+   * @param str The string to test
+   * @return    Boolean indicating if the string represents a GRIB file
+   */
+  def isGRIB(str: String): Boolean = {
+    return str.substring(0, Math.min(str.length(), 4)).equals("GRIB")
+  }
 }
