b8f2f3f8e07b89be7d1cd4eb855bde91a6338517
Adrien Merat <adrien.merat@epita.fr>
2022-01-18 23:55:52 +0100

    [EDIT] Add manageRequest method

--- src/main/scala/com/meteofrance/Service.scala
+++ src/main/scala/com/meteofrance/Service.scala
@@ -1,5 +1,6 @@
 package com.meteofrance
 
+import com.meteofrance.exceptions.InvalidResponseBodyTypeException
 import com.typesafe.scalalogging.Logger
 import org.apache.commons.io.FileUtils
 import org.apache.spark.sql.DataFrame
@@ -74,6 +75,38 @@ object Service {
     } catch {
       case e: Throwable => e.printStackTrace()
         return df
+    }
+  }
+
+  /**
+   * This class aim to call the API on the url parameter and send the received data to
+   * the GRIB2 parser in order to write them on the DataFrame df given in arguments as
+   * a new column called by the string colName.
+   *
+   * @param url     URL adress of the API endpoint to call
+   * @param df      Dataframe holding the actual state of the data we're collecting
+   *                from the API and where the function will store the data from url
+   * @param colName Name of the column to add to the dataframe
+   *
+   * @return DataFrame with the new column filled with the data collected from the
+   *         to the given URL request
+   */
+  @throws (classOf[InvalidResponseBodyTypeException])
+  @throws (classOf[IllegalArgumentException])
+  def manageRequest(url: String, df: DataFrame, colName: String) : DataFrame = {
+    if (url == null || url.equals("") || df == null || colName == null || colName.equals("")) {
+      throw new IllegalArgumentException("manageRequest(url, df, colName) cannot contain empty or null arguments")
+    }
+
+    val body: Array[Byte] = Controller.requestTo(url)
+    val content: String = new String(body)
+
+    if (isGRIB(content)) {
+      logger.info("Content is in GRIB format")
+      return readGRIB(body, df, colName)
+
+    } else {
+      throw new InvalidResponseBodyTypeException("Response's body is not in JSON nor XML format")
     }
   }
 }
