91afcded2bef9f8b611d03fc2138796e92acf476
Adrien Merat <adrien.merat@epita.fr>
2022-01-18 23:43:26 +0100

    [EDIT] Add documentation on NullHttpResponseException.scala

--- src/main/scala/com/meteofrance/exceptions/NullHttpResponseException.scala
+++ src/main/scala/com/meteofrance/exceptions/NullHttpResponseException.scala
@@ -1,5 +1,8 @@
 package com.meteofrance.exceptions
 
+/**
+ * Occurs when the server returns a null response body or when the request fails.
+ */
 final case class NullHttpResponseException(private val message: String = "",
                                            private val cause: Throwable = None.orNull)
   extends Exception(message, cause)
