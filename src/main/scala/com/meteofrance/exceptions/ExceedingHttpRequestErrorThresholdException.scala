546d7a9475efcab132b5141f764845fd28875326
Adrien Merat <adrien.merat@epita.fr>
2022-01-18 23:35:57 +0100

    [EDIT] Add documentation on ExceedingHttpRequestErrorThresholdException.scala

--- src/main/scala/com/meteofrance/exceptions/ExceedingHttpRequestErrorThresholdException.scala
+++ src/main/scala/com/meteofrance/exceptions/ExceedingHttpRequestErrorThresholdException.scala
@@ -1,5 +1,8 @@
 package com.meteofrance.exceptions
 
+/**
+ * Occurs when exceeding the maximum authorized iteration to fetch data or a new token.
+ */
 final case class ExceedingHttpRequestErrorThresholdException(private val message: String = "",
                                                              private val cause: Throwable = None.orNull)
   extends Exception(message, cause)
